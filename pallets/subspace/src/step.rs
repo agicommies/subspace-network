f6ea9f23eba219146c9a2b5428f74169c8102d82
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-05-04 11:13:57 -0300

    refac(pallets/subspace): remove old code
    Signed-off-by: Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>

--- pallets/subspace/src/step.rs
+++ pallets/subspace/src/step.rs
@@ -113,13 +113,6 @@ failed to run yuma consensus algorithm: {err:?}, skipping this block. \
         let (token_emission, founder_emission) =
             Self::calculate_founder_emission(netuid, token_emission, &founder_key);
 
-        if netuid == 0 {
-            // Update global treasure
-            GlobalDaoTreasury::<T>::mutate(|global_treasure| {
-                *global_treasure += founder_emission;
-            });
-        }
-
         // STAKE
         let uid_key_tuples: Vec<(u16, T::AccountId)> = Self::get_uid_key_tuples(netuid);
         let total_stake_u64: u64 = Self::get_total_subnet_stake(netuid).max(1);
