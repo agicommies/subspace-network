c0e3f2d7e1251cb86209497daec53bfcbe4f5c64
YourUsername <zimulajan0@gmail.com>
2024-07-01 20:26:55 +0000

    feat: branch compatibility

--- pallets/subspace/src/migrations.rs
+++ pallets/subspace/src/migrations.rs
@@ -23,7 +23,8 @@ pub fn ss58_to_account_id<T: Config>(
 
 pub mod v12 {
     use super::*;
-    use frame_support::{traits::OnRuntimeUpgrade, weights::Weight};
+    use frame_support::{traits::OnRuntimeUpgrade, weights::Weight, StorageDoubleMap};
+    use pallet_governance_api::{GovernanceConfiguration, VoteMode};
     use sp_std::collections::btree_map::BTreeMap;
 
     pub mod old_storage {
@@ -125,9 +126,185 @@ pub mod v12 {
             old_storage::TotalStake::<T>::remove_all(None);
             log::info!("Migrated TotalStake");
 
-            StorageVersion::new(12).put::<Pallet<T>>();
+            // Subnet netuid migration
+            /*
+            ====================
+            Currently
+            ====================
+            Storages are structured like this
+            NETUID     | NAME
+            - Subnet 0 | Linear
+            - Subnet 1 | Zangief subnet
+            - Subnet 2 | Comchat subnet
 
+            ====================
+            After migration
+            ====================
+
+            NETUID     | NAME
+            - Subnet 0 | Rootnet
+            - Subnet 1 | Treasury subnet
+            - Subnet 2 | Linear Subnet
+
+            ------------------------------------------------------------
+
+            Question is, what do we do with subnet 1,2
+
+            Determine the free netuid values:
+
+            let netuid = netuid.unwrap_or_else(|| match SubnetGaps::<T>::get().first().copied() {
+                Some(removed) => removed,
+                None => TotalSubnets::<T>::get(),
+            });
+
+            and move the subnet 1 + 2, to these free netuid spots.
+
+            When you have done this, move subnet 0 to subnet 2
+
+            And insert the new rootnet into SN0, and SN1 for treasury subnet.
+
+            ------------------------------------------------------------
+
+            Both new subnets, Root & Treasury, have to be registered with specific parameters:
+
+            -------------------------
+
+            - Rootnet:
+            set_max_allowed_uids to the number of allowed rootnet validators.
+            set vote mode to vote
+            set founder fee to 0
+            set subnet consensus type to Root
+
+            -------------------------
+            - Treasury subnet:
+            set_founder the treasury account
+            set_founder_fee to 100
+            set_vote_mode to vote
+            set max_allowed_uids to 0
+            set subnet consensus type to Treasury
+
+             -------------------------
+
+            - For linear don't change any parameters, just set
+            the consensus type to Linear.
+
+            */
+
+            transfer_subnet::<T>(1, None);
+            transfer_subnet::<T>(2, None);
+            transfer_subnet::<T>(0, Some(2));
+
+            // Rootnet configuration
+            const ROOTNET_ID: u16 = 0;
+            SubnetNames::<T>::set(ROOTNET_ID, b"Rootnet".to_vec());
+            MaxAllowedUids::<T>::set(ROOTNET_ID, 256);
+            MaxAllowedValidators::<T>::set(ROOTNET_ID, Some(256));
+            set_vote_mode::<T>(ROOTNET_ID);
+            FounderShare::<T>::set(ROOTNET_ID, 0);
+            // how to SubnetConsensusType::<T>::set(ROOTNET_ID, SubnetConsensus::Root);
+
+            // Treasury subnet configuration
+            const TREASURYNET_ID: u16 = 1;
+            SubnetNames::<T>::set(0, b"Treasury".to_vec());
+            set_vote_mode::<T>(TREASURYNET_ID);
+            Founder::<T>::set(TREASURYNET_ID, T::get_dao_treasury_address());
+            FounderShare::<T>::set(TREASURYNET_ID, u16::MAX);
+            // how to SubnetConsensusType::<T>::set(TREASURYNET_ID, SubnetConsensus::Treasury);
+
+            // Linear subnet configuration
+            const LINEARNET_ID: u16 = 2;
+            // how to SubnetConsensusType::<T>::set(LINEARNET_ID, SubnetConsensus::Linear);
+
+            StorageVersion::new(12).put::<Pallet<T>>();
             T::DbWeight::get().reads_writes(1, 1)
         }
+    }
+
+    fn set_vote_mode<T: Config>(subnet_id: u16) {
+        let mut rootnet_governance_configuration =
+            T::get_subnet_governance_configuration(subnet_id);
+        rootnet_governance_configuration.vote_mode = VoteMode::Vote;
+
+        if let Err(err) =
+            T::update_subnet_governance_configuration(subnet_id, rootnet_governance_configuration)
+        {
+            log::error!(
+                "could not update ROOTNET governance configuration: {:?}",
+                err
+            );
+        };
+    }
+
+    fn transfer_subnet<T: Config>(current_subnet_id: u16, target_subnet_id: Option<u16>) {
+        let target_subnet_id =
+            target_subnet_id.unwrap_or(match SubnetGaps::<T>::get().first().copied() {
+                Some(removed) => removed,
+                None => TotalSubnets::<T>::get(),
+            });
+
+        let curr = current_subnet_id;
+        let target = target_subnet_id;
+
+        migrate_double_map!(T, Bonds, curr, target);
+        migrate_map!(T, BondsMovingAverage, curr, target);
+        migrate_map!(T, ValidatorPermits, curr, target);
+        migrate_map!(T, ValidatorTrust, curr, target);
+        migrate_map!(T, PruningScores, curr, target);
+        migrate_map!(T, MaxAllowedValidators, curr, target);
+        migrate_map!(T, Consensus, curr, target);
+        migrate_map!(T, Active, curr, target);
+        migrate_map!(T, Rank, curr, target);
+        migrate_map!(T, RegistrationsThisInterval, curr, target);
+        migrate_map!(T, Burn, curr, target);
+        migrate_map!(T, MaximumSetWeightCallsPerEpoch, curr, target);
+        migrate_double_map!(T, SetWeightCallsPerEpoch, curr, target);
+        migrate_map!(T, TargetRegistrationsInterval, curr, target);
+        migrate_map!(T, TargetRegistrationsPerInterval, curr, target);
+        migrate_map!(T, AdjustmentAlpha, curr, target);
+        migrate_map!(T, N, curr, target);
+        migrate_map!(T, Founder, curr, target);
+        migrate_map!(T, IncentiveRatio, curr, target);
+        migrate_map!(T, MaxAllowedUids, curr, target);
+        migrate_map!(T, ImmunityPeriod, curr, target);
+        migrate_map!(T, MinAllowedWeights, curr, target);
+        migrate_map!(T, MinStake, curr, target);
+        migrate_map!(T, MaxRegistrationsPerInterval, curr, target);
+        migrate_map!(T, MaxWeightAge, curr, target);
+        migrate_map!(T, MaxAllowedWeights, curr, target);
+        migrate_map!(T, TrustRatio, curr, target);
+        migrate_map!(T, Tempo, curr, target);
+        migrate_map!(T, FounderShare, curr, target);
+        migrate_double_map!(T, Uids, curr, target);
+        migrate_double_map!(T, Keys, curr, target);
+        migrate_double_map!(T, Name, curr, target);
+        migrate_double_map!(T, Address, curr, target);
+        migrate_double_map!(T, Metadata, curr, target);
+        migrate_map!(T, Incentive, curr, target);
+        migrate_map!(T, Trust, curr, target);
+        migrate_map!(T, Dividends, curr, target);
+        migrate_map!(T, Emission, curr, target);
+        migrate_map!(T, LastUpdate, curr, target);
+        migrate_double_map!(T, RegistrationBlock, curr, target);
+        migrate_double_map!(T, Weights, curr, target);
+        migrate_double_map!(T, DelegationFee, curr, target);
+        migrate_double_map!(T, DelegationFee, curr, target);
+
+        // TODO: StakeFrom, StakeTo (?)
+    }
+
+    #[macro_export]
+    macro_rules! migrate_double_map {
+        ($gen:ident, $map:ident, $curr_id:ident, $target_id:ident) => {
+            for k2 in $map::<$gen>::iter_key_prefix(&$curr_id) {
+                $map::<$gen>::swap(&$curr_id, &k2, &$target_id, &k2);
+            }
+        };
+    }
+
+    #[macro_export]
+    macro_rules! migrate_map {
+        ($gen:ident, $map:ident, $curr_id:ident, $target_id:ident) => {
+            $map::<$gen>::swap($curr_id, $target_id);
+        };
     }
 }
